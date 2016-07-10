633345511b7ac1476b568124343bc3597f3fa503
unknown <bennyzhao@live.ca>
2016-07-10 00:26:31 -0400

    implemented login and dashboard using gentelella

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,6 @@ router.get('/', function (req, res) {
       res.render('index', { user : req.user });
   });
 
-/*
-router.get('/register', function(req, res) {
-      res.render('register', { });
-  });
-*/
-
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
@@ -28,12 +22,6 @@ router.post('/register', function(req, res) {
         });
     });
   });
-
-/*
-router.get('/login', function(req, res) {
-      res.render('login', { user : req.user });
-  });
-*/
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
       res.redirect('home');
